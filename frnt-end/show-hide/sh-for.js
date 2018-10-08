1e98e3bc2f3fe1c79353d1cc48e5040bb7bba70f
AlexBerg82 <Username for 'https://github.com': BerdnykAleks@yahoo.comgit config --global user.email BerdnykAleks@yahoo.comgit config --global user.email>
2018-10-08 18:16:26 +0300

    add-show

---
+++ frnt-end/show-hide/sh-for.js
@@ -0,0 +1,31 @@
+﻿(function($){
+	$.fn.showAll = function(params){
+
+		for(var i = 0; i < $(this.selector)[0].children.length; i++){
+			if(i + 1 > params.quantity){
+				$($(this.selector)[0].children[i]).css('display','none');
+			}
+		}
+
+		var cnt = $(this.selector)[0];
+		var qa = params.quantity;
+		
+		$(params.button).click(function(){
+			for(var i = 0; i < cnt.children.length; i++){
+				var li = cnt.children[i];
+
+				if($(li).css('display') == 'none'){
+					$(li).css('display','inline-block');
+					$($(params.button)[0]).text(params.buttonTextHide);
+				} else {
+					if(i > params.quantity - 1){
+						$(li).css('display','none');
+						$($(params.button)[0]).text(params.buttonTextShow);
+						li = null;
+					}
+				}
+			}
+		});
+
+	};
+})(jQuery);
---
+++ frnt-end/show-hide/sh.js
@@ -0,0 +1,33 @@
+﻿(function($){
+	$.fn.showAll = function(params){
+
+		for(var i = 0; i < $(this.selector)[0].children.length; i++){
+			if(i + 1 > params.quantity){
+				$($(this.selector)[0].children[i]).css('display','none');
+			}
+		}
+
+		var cnt = $(this.selector)[0];
+		var qa = params.quantity;
+		
+		$(params.button).click(function(){
+			for(var key in cnt.children){
+				var li = cnt.children[key];
+
+				if(key != 'length' && key != 'item' && key != 'namedItem'){
+					if($(li).css('display') == 'none'){
+						$(li).css('display','inline-block');
+						$($(params.button)[0]).text(params.buttonTextHide);
+					} else {
+						if(key > params.quantity - 1){
+							$(li).css('display','none');
+							$($(params.button)[0]).text(params.buttonTextShow);
+							li = null;
+						}
+					}
+				}
+			}
+		});
+
+	};
+})(jQuery);
